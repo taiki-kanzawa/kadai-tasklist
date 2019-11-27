9a0bdf73b10366971a94b1ed4d9c787994a4a116
takahiro.sawakami <takahirosawakami81@gmail.com>
2019-11-27 08:17:24 +0000

    login authentication

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@
 
 Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+// ログイン認証付きのルーティング
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController');
+});
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
